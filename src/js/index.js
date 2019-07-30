c9dbcc75704d34fb5f0b4b374918242108ecc4ca
hyl-nancy <1795458359@qq.com>
2019-07-30 19:00:22 +0800

    header footer

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+require(['./config'],() => {
+  require(['header','footer'], () => {
+    class Index {
+      constructor()
+      
+    }
+    new Index()
+  })
+})
---
+++ src/js/modules/header.js
@@ -0,0 +1,12 @@
+define(['jquery'], () =>{
+  class Header{
+    constructor(){
+      this.load()
+    }
+    load(){
+      $('header').load('/html/modules/header.html', () => {
+      })
+    }
+  }
+  return new Header()
+});
