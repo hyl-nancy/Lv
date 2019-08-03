809a81534d4adc5facafaa0bde638d6b8e3ae96a
hyl-nancy <1795458359@qq.com>
2019-08-03 15:39:42 +0800

    详情页渲染

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,5 +1,5 @@
 require(['./config'],() => {
-  require(['template','header','footer','url'], (template) => {
+  require(['template','header','footer'], (template) => {
     class Detail {
       constructor(){
         this.getData()
@@ -7,7 +7,12 @@ require(['./config'],() => {
       getData(){
        const id = location.search.slice(4)
        console.log(id)
-       $.get('http://rap2api.taobao.org/app/mock/226928/detailurl')
+       $.get()
+       if(resp.code === 200){
+         let {detail}=resp.body
+        $.get('http://rap2api.taobao.org/app/mock/226928/detailurl',{id},resp =>{
+          this.detail = {...detail,id}
+       })
       }
     }
     new Detail()
